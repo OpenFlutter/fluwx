5451e5971dc95c382dc4ce451ea1052b224fbd94
JarvanMo <jarvan.mo@gmail.com>
2018-08-25 21:26:09 +0800

    export WeChatSendAuthModel

--- lib/fluwx.dart
+++ lib/fluwx.dart
@@ -6,3 +6,4 @@ export 'src/models/wechat_share_models.dart';
 export 'src/wechat_reponse.dart';
 export 'src/wechat_scene.dart';
 export 'src/models/wechat_pay_model.dart';
+export 'src/models/wechat_send_auth_model.dart';

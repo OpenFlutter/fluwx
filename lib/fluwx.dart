f8441ec07f80f0f7d90cbb7bf72561307327b12c
JarvanMo <jarvan.mo@gmail.com>
2018-08-27 21:51:51 +0800

    refactor: response api changed

--- lib/fluwx.dart
+++ lib/fluwx.dart
@@ -7,3 +7,4 @@ export 'src/wechat_reponse.dart';
 export 'src/wechat_scene.dart';
 export 'src/models/wechat_pay_model.dart';
 export 'src/models/wechat_send_auth_model.dart';
+export 'src/models/wechat_response.dart';

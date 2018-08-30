fd66cca73da10e383d1e86f2757b5be722f31ad1
JarvanMo <jarvan.mo@gmail.com>
2018-08-30 09:21:26 +0800

    iOS:enable mta support

--- ios/Classes/handler/FluwxWXApiHandler.m
+++ ios/Classes/handler/FluwxWXApiHandler.m
@@ -28,7 +28,7 @@
     }
 
 
-    isWeChatRegistered = [WXApi registerApp:appId];
+    isWeChatRegistered = [WXApi registerApp:appId enableMTA:[call.arguments[@"enableMTA"] boolValue]];
     UInt64 typeFlag = MMAPP_SUPPORT_TEXT | MMAPP_SUPPORT_PICTURE | MMAPP_SUPPORT_LOCATION | MMAPP_SUPPORT_VIDEO |MMAPP_SUPPORT_AUDIO | MMAPP_SUPPORT_WEBPAGE | MMAPP_SUPPORT_DOC | MMAPP_SUPPORT_DOCX | MMAPP_SUPPORT_PPT | MMAPP_SUPPORT_PPTX | MMAPP_SUPPORT_XLS | MMAPP_SUPPORT_XLSX | MMAPP_SUPPORT_PDF;
 
     [WXApi registerAppSupportContentFlag:typeFlag];

956734399b85683d915f036982ec81487130bafc
JarvanMo <jarvan.mo@gmail.com>
2018-09-29 14:14:30 +0800

    rm WeChatResponseType

--- lib/src/models/wechat_response.dart
+++ lib/src/models/wechat_response.dart
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-enum WeChatResponseType { SHARE, AUTH, PAYMENT }
+//enum WeChatResponseType { SHARE, AUTH, PAYMENT }
 
 /// response data from WeChat.
 //class WeChatResponse {

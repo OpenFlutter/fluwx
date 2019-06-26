88cf306d1457bb6c7629d77280bd4b7b9e2c0dff
JarvanMo <jarvan.mo@gmail.com>
2019-06-26 17:32:41 +0800

    ios内部处理url

--- ios/Classes/FluwxPlugin.m
+++ ios/Classes/FluwxPlugin.m
@@ -129,13 +129,15 @@ FluwxAutoDeductHandler *_fluwxAutoDeductHandler;
 }
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-    return [WXApi handleOpenURL:url delegate:[FluwxResponseHandler defaultManager]];
+    [WXApi handleOpenURL:url delegate:[FluwxResponseHandler defaultManager]];
+    return NO;
 }
 // NOTE: 9.0以后使用新API接口
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
 {
 
-    return [WXApi handleOpenURL:url delegate:[FluwxResponseHandler defaultManager]];
+    [WXApi handleOpenURL:url delegate:[FluwxResponseHandler defaultManager]]
+    return NO;
 }
 
 

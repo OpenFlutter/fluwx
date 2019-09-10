56defa467dd3075acdb010c2a135b165cdb20fd2
JarvanMo <jarvan.mo@gmail.com>
2019-09-10 20:48:35 +0800

    bump to 1.0.6

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mo/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mo/Code/Flutter/fluwx/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mo/Development/flutter/bin/cache/artifacts/engine/ios"

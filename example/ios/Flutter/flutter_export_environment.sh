f4e3fbe578498eba04d138d3cdb5be3a23f585e1
JarvanMo <jarvan.mo@gmail.com>
2019-10-28 19:53:44 +0800

    fix #146

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,8 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mo/Development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/mo/Code/Flutter/fluwx/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/mo/Code/Flutter/fluwx/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/mo/Development/flutter/bin/cache/artifacts/engine/ios"
+export "TRACK_WIDGET_CREATION=true"

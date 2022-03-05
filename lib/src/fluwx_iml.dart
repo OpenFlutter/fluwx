cce7f98b5005ac9a463c2b4c9f85193ea0160705
JarvanMo <jarvan.mo@gmail.com>
2022-03-05 11:04:18 +0800

    merge master

--- lib/src/fluwx_iml.dart
+++ lib/src/fluwx_iml.dart
@@ -267,8 +267,6 @@ Future<bool> openWeChatCustomerServiceChat({required String url, required String
   return await _channel
       .invokeMethod("openWeChatCustomerServiceChat", {"corpId": corpId, "url": url});
 }
-<<<<<<< HEAD
-=======
 
 // see https://pay.weixin.qq.com/wiki/doc/apiv3_partner/Offline/apis/chapter6_2_1.shtml
 Future<bool> openWeChatBusinessView({required String businessType, required String query}) async {
@@ -279,4 +277,3 @@ Future<bool> openWeChatBusinessView({required String businessType, required Stri
 Future<bool> checkSupportOpenBusinessView() async {
   return await _channel.invokeMethod("checkSupportOpenBusinessView");
 }
->>>>>>> 6e07c71 (支持App打开微信支付分)

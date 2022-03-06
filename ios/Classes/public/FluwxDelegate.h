89078e5cf9cd07089a8fa816207757ea02cd993b
JarvanMo <jarvan.mo@gmail.com>
2022-03-06 21:15:47 +0800

    add FluwxDelegate in iOS

---
+++ ios/Classes/public/FluwxDelegate.h
@@ -0,0 +1,12 @@
+//
+//  FluwxDelegate.h
+//  Pods
+//
+//  Created by Mo on 2022/3/6.
+//
+#import <Foundation/Foundation.h>
+
+@interface FluwxDelegate : NSObject
+
+- (void)registerWxAPI:(NSString *)appId universalLink:(NSString *)universalLink;
+@end

542c531de70bbb7fb5fb0ac0beb7e985ad0268ad
JarvanMo <jarvan.mo@gmail.com>
2023-09-18 18:59:31 +0800

    remove useless ut

--- android/src/test/kotlin/com/jarvan/fluwx/FluwxPluginTest.kt
+++ android/src/test/kotlin/com/jarvan/fluwx/FluwxPluginTest.kt
@@ -1,9 +1,5 @@
 package com.jarvan.fluwx
 
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import kotlin.test.Test
-import org.mockito.Mockito
 
 /*
  * This demonstrates a simple unit test of the Kotlin portion of this plugin's implementation.
@@ -14,14 +10,5 @@ import org.mockito.Mockito
  */
 
 internal class FluwxPluginTest {
-  @Test
-  fun onMethodCall_getPlatformVersion_returnsExpectedValue() {
-    val plugin = FluwxPlugin()
 
-    val call = MethodCall("getPlatformVersion", null)
-    val mockResult: MethodChannel.Result = Mockito.mock(MethodChannel.Result::class.java)
-    plugin.onMethodCall(call, mockResult)
-
-    Mockito.verify(mockResult).success("Android " + android.os.Build.VERSION.RELEASE)
-  }
 }
